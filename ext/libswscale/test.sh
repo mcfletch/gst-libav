43f82f206fc8467a769272f90cb7b014346c8ac8
Mike C. Fletcher <mcfletch@vrplumber.com>
2018-05-17 11:56:51 -0400

    Add a dumb test-script for testing the component...

---
+++ ext/libswscale/test.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+set -e
+make clean
+make
+sudo cp .libs/libgstavvideoscale.so /usr/lib/x86_64-linux-gnu/gstreamer-1.0/
+# GST_CAPS:5,basetransform:5,avvideoscale:5
+gdb gst-launch-1.0 --args \
+gst-launch-1.0 --gst-debug=video-info:5,3 \
+    videotestsrc ! \
+    capsfilter name=biginput caps="video/x-raw,format=I420,width=1920,height=1080,framerate=30001/1000" ! \
+    avvideoscale ! \
+    capsfilter name=smalloutput caps="video/x-raw,width=720,height=480" ! \
+    fakesink
